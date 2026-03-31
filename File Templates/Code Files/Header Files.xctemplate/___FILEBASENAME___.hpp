8679f82ba45ada5d1c4edaf183305027d4e453c8
Andrew Grant <14335009+AndrewGrant31@users.noreply.github.com>
2026-03-31 14:45:26 -0400

    Add class template for ___FILEBASENAME___

--- File Templates/Code Files/Header Files.xctemplate/___FILEBASENAME___.hpp
+++ File Templates/Code Files/Header Files.xctemplate/___FILEBASENAME___.hpp
@@ -5,5 +5,14 @@
 // if not, just complete the path until the error message goes away
 #include "pch.hh"
 
+namespace NAMESPACE
+{
+class ___FILEBASENAME___
+{
+private:
 
+public:
+    explicit ___FILEBASENAME___() = default;
+};
+}
 
